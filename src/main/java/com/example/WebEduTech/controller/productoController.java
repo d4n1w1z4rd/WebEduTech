d75a3bb50b696d8fd12b328f80a538176e0f037f
Dsim-RgB <di.simon@duocuc.cl>
2025-05-28 00:51:34 -0400

    Update productoController.java

--- src/main/java/com/example/WebEduTech/controller/productoController.java
+++ src/main/java/com/example/WebEduTech/controller/productoController.java
@@ -1,11 +1,19 @@
 package com.example.WebEduTech.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.WebEduTech.model.Producto;
 import com.example.WebEduTech.service.ProductoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/productos")
@@ -43,4 +51,6 @@ public class productoController {
     public int totalProductosV2(){
         return productoService.totalProductosV2();
     }
+
+    
 }

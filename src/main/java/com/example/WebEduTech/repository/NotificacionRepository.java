e1f9742354d996e2057e62e3dc374dfa26b80cbc
Dsim-RgB <di.simon@duocuc.cl>
2025-05-27 22:49:16 -0400

    Repository Notificacion
    Se completa repository notificacion

--- src/main/java/com/example/WebEduTech/repository/NotificacionRepository.java
+++ src/main/java/com/example/WebEduTech/repository/NotificacionRepository.java
@@ -1,5 +1,24 @@
 package com.example.WebEduTech.repository;
 
+import com.example.EduTech_Innovators_SPA.model.Notificacion;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class NotificacionRepository {
-    
+    private List<Notificacion> listaNotificaciones = new ArrayList<>();
+
+    public NotificacionRepository() {
+        // Agregar notificaciones por defecto
+        listaNotificaciones.add(new Notificacion(1, "Bienvenido a EduTech Innovators!", 1));
+        listaNotificaciones.add(new Notificacion(2, "Tu curso ha sido actualizado.", 2));
+        listaNotificaciones.add(new Notificacion(3, "Nuevo mensaje de tu instructor.", 1));
+        listaNotificaciones.add(new Notificacion(4, "Tu tarea ha sido calificada.", 3));
+    }
+
+    public List<Notificacion> obtenerNotificaciones(){
+        return listaNotificaciones;
+    }
 }

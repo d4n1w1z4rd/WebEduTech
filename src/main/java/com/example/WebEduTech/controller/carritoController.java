0101a6892fbbd49eefe35c74ee5cadf4201c080a
Dsim-RgB <di.simon@duocuc.cl>
2025-05-28 01:49:49 -0400

    metodo agregar con stock
    se le agrega un get stock y un set stock para descontar 1 luego dar uptadeProducto

--- src/main/java/com/example/WebEduTech/controller/carritoController.java
+++ src/main/java/com/example/WebEduTech/controller/carritoController.java
@@ -1,17 +1,18 @@
 package com.example.WebEduTech.controller;
 
-import com.example.WebEduTech.model.Producto;
-import com.example.WebEduTech.service.ProductoService;
-
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.WebEduTech.model.Producto;
+import com.example.WebEduTech.service.ProductoService;
 
 
 @RestController
@@ -26,8 +27,14 @@ public class carritoController {
     public String agregarProducto(@PathVariable int id){
         Producto producto = productoser.getProductoId(id);
         if (producto != null){
-            carrito.add(producto);
-            return "Producto agregado al carrito" + producto.getTitulo();
+            if (producto.getStock()>0){
+                producto.setStock(producto.getStock()-1);
+                productoser.updateProducto(producto);
+                carrito.add(producto);
+                return "Producto agregado al carrito" + producto.getTitulo();
+            } else {
+                return "producto sin Stock";
+            }
         }
         return "Producto no fue encontrado";
     }

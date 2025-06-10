f8aaee55777ca4dafc474e06be266c7722055652
Dsim-RgB <di.simon@duocuc.cl>
2025-06-10 12:15:30 -0400

    tests

---
+++ src/test/java/com/example/WebEduTech/controller/usuarioControllerIntegrationTest.java
@@ -0,0 +1,20 @@
+package com.example.WebEduTech.controller;
+//importar el backend de la clase usuario
+import com.example.WebEduTech.model.usuario;
+import com.example.WebEduTech.service.usuarioService;
+
+//Importar ObjectMapper para convertir objetos en JSON
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+//Importar las anotaciones de pruebas JUnit
+import org.junit.jupiter.api.Test;
+
+//Importar las anotaciones de Spring para inyectar las dependencias Maven
+import org.springframework.beans.factory.annotation.Autowired;
+
+//Importar de la anotacion para las pruebas de los controladores Web
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+
+public class usuarioControllerIntegrationTest {
+    
+}
